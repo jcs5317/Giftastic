dd4f1c43b539f0b4bc83e3a766ddd025c72f72a7
jordan <jcs5317@gmail.com>
2019-04-29 17:10:09 -0700

    latest

--- w.js
+++ w.js
@@ -1,23 +1,17 @@
-for (var i = 0; i < response.data.length; i++) {
-
-          // Creating and storing a div tag
-          var feelingDiv = $("<div>");
-
-          // Creating a paragraph tag with the result item's rating
-          var p = $("<p>").text("Rating: " + response.data[i].rating);
-
-          // Creating and storing an image tag
-          var feelingImage = $("<img>");
-          // Setting the src attribute of the image to a property pulled off the result item
-          feelingImage.attr("src", results[i].images.fixed_height.url);
-
-          // Appending the paragraph and image tag to the feelingDiv
-          feelingDiv.append(p);
-          feelingDiv.append(feelingImage);
-
-          // Prependng the animalDiv to the HTML page in the "#gifs-appear-here" div
-          $("#gifs-appear-here").prepend(feelingDiv);
-        }
+$(document).on("click", "img", function () {
+    // The attr jQuery method allows us to get or set the value of any attribute on our HTML element
+    var state = $(this).attr("data-state");
+    // If the clicked image's state is still, update its src attribute to what its data-animate value is.
+    // Then, set the image's data-state to animate
+    // Else set src to the data-still value
+    if (state === "still") {
+      $(this).attr("src", $(this).attr("data-animate"));
+      $(this).attr("data-state", "animate");
+    } else {
+      $(this).attr("src", $(this).attr("data-still"));
+      $(this).attr("data-state", "still");
+    }
+  });
 
 /*<script>
      (document).ready(function() {
